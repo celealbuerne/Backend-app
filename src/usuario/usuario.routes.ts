881cf167231bfbe64212544d261c68563fb11fd2
franstar55 <26francor@gmail.com>
2026-08-19 22:47:51 -0300

    middlewares de sanitizacion usuario

--- src/usuario/usuario.routes.ts
+++ src/usuario/usuario.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UsuarioController } from "./usuario.controller.js";
+import * as md from './usuario.middleware.js'
 
 const usuarioRouter = Router();
 const c = new UsuarioController();
@@ -7,6 +8,6 @@ const c = new UsuarioController();
 // CRUD
 usuarioRouter.get('/', c.findAll);
 usuarioRouter.get('/:id', c.getOne);
-usuarioRouter.post('/', /* md.sanitizeInput, md.validarCrearDatos,*/ c.saveOne);
+usuarioRouter.post('/', md.sanitizeInput, md.validarCrearDatos, c.saveOne);
 
 export default usuarioRouter;
